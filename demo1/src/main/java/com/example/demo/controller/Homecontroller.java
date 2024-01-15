026487f72e59efedba7f4edfd86457ae0f36de8e
minsudex <minsuf4@naver.com>
2024-01-15 15:57:30 +0900

    student 상세조회

--- demo1/src/main/java/com/example/demo/controller/Homecontroller.java
+++ demo1/src/main/java/com/example/demo/controller/Homecontroller.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -90,5 +92,17 @@ public class Homecontroller {
 		model.addAttribute("demo", demoDTO);
 		return "model3";
 	}
-}
-		
+	
+	@GetMapping("/model4")
+	public String model4(Model model) {
+		List<DemoDTO> list = new ArrayList<>();
+		for(int i=1; i<=10; i++) {
+			DemoDTO demoDTO = new DemoDTO();
+			demoDTO.setName("name" + i);
+			demoDTO.setAge(i);
+			list.add(demoDTO);
+		}
+		model.addAttribute("list", list);
+		return "model4";
+	}
+	}
